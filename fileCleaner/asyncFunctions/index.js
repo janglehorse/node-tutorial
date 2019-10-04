a9295db7f40f8c930f670206a59a49621b127a33
J. Caleb Travers <jcalebtravers@icloud.com>
2019-10-04 14:50:22 -0500

    use index arg

--- fileCleaner/asyncFunctions/index.js
+++ fileCleaner/asyncFunctions/index.js
@@ -4,22 +4,45 @@ const fs = require('fs');
 const ms1Day = 24*60*60*1000;
 const LOGS_PATH = path.join(__dirname, '../logs');
 
-module.exports = function() {
-    return new Promise((resolve, reject) => {
-        const filename = path.join(LOGS_PATH, `log_${i}`);
-        data = `This is log ${i}`;
-        fs.writeFile(filename, data, (err) => {
-            if (err) {
-                reject(err);
-            }
-            const time = (Date.now() - i*ms1Day)/1000;
-            fs.utimes(filename, time, time, (err) => {
+const ONE_WEEK_AGO = new Date();
+ONE_WEEK_AGO.setDate(ONE_WEEK_AGO.getDate() - 7); 
+
+module.exports = {
+    writeFileDecrementTimeStamp: function(i) {
+        return new Promise((resolve, reject) => {
+            const filename = path.join(LOGS_PATH, `log_${i}`);
+            data = `This is log ${i}`;
+            fs.writeFile(filename, data, (err) => {
                 if (err) {
                     reject(err);
                 }
-                console.log(`wrote '${filename}' successfully`)
-                resolve();
+                const time = (Date.now() - i*ms1Day)/1000;
+                fs.utimes(filename, time, time, (err) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    console.log(`wrote '${filename}' successfully`)
+                    resolve();
+                });
             });
         });
-    });
+    },
+    cleanup: function() {
+        const files = fs.readdirSync(LOGS_PATH);
+        files.forEach(filename => {
+            fs.stat(path.join(LOGS_PATH, filename), (err, stats) => {
+                if (err) throw err;
+                if (stats.birthtime < ONE_WEEK_AGO) {
+                    console.log(`${filename} is too old. Deleting...`)
+                    fs.unlink(path.join(LOGS_PATH, filename), err => {
+                        if (err) {
+                            console.log(`Couldn't delete ${path.join(LOGS_PATH, filename)}: ${err}`);
+                            throw err;
+                        }
+                        console.log(`deleted ${filename}`)
+                    });
+                }
+            });
+        });
+    }
 }
--- fileCleaner/fileCleanerAsync.js
+++ fileCleaner/fileCleanerAsync.js
@@ -1,41 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-const writeFileDecrementTimeStamp = require('./asyncFunctions');
-
-const dirname = path.join(__dirname, 'logs');
-
-/*
-Writes files, alters their timestamps and then deletes those older than 7 days, asynchronously
-*/
-const ONE_WEEK_AGO = new Date();
-ONE_WEEK_AGO.setDate(ONE_WEEK_AGO.getDate() - 7); 
-
-function cleanup() {
-    const files = fs.readdirSync(dirname);
-    files.forEach(filename => {
-        fs.stat(path.join(dirname, filename), (err, stats) => {
-            if (err) throw err;
-            if (stats.birthtime < ONE_WEEK_AGO) {
-                console.log(`${filename} is too old. Deleting...`)
-                fs.unlink(path.join(dirname, filename), err => {
-                    if (err) {
-                        console.log(`Couldn't delete ${path.join(dirname, filename)}: ${err}`);
-                        throw err;
-                    }
-                    console.log(`deleted ${filename}`)
-                });
-            }
-        });
-    });
-}
+const asyncHelpers = require('./asyncFunctions');
 
 async function main() {
     for (i = 0; i < 10; i++) {
-        await writeFileDecrementTimeStamp()
+        await asyncHelpers.writeFileDecrementTimeStamp(i)
             .catch(e => {console.log('ERROR: ', e)});
     }
-    cleanup();
+    asyncHelpers.cleanup();
 }
 
 main();
