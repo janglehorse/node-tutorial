82650bb0c8f23da28bfd926ce55d9046ba20de45
J. Caleb Travers <jcalebtravers@icloud.com>
2019-10-04 15:05:49 -0500

    Clean sync logic

---
+++ fileCleaner/blockingFunctions/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const dirname = path.join(__dirname, '../logs');
+
+const ONE_WEEK_AGO = new Date();
+ONE_WEEK_AGO.setDate(ONE_WEEK_AGO.getDate() - 7);
+const ms1Day = 24*60*60*1000;
+
+module.exports = {
+    writeFileDecrementTimeStamp: function(i) {
+        const filename = path.join(dirname, `log_${i}`);
+        data = `This is log ${i}`;
+        fs.writeFileSync(filename, data);
+        console.log('successfully wrote ', filename);
+        const time = (Date.now() - i*ms1Day)/1000;
+        fs.utimesSync(filename, time, time);
+    },
+    cleanup: function() {
+        const files = fs.readdirSync(dirname);
+        files.forEach(filename => {
+            const stats = fs.statSync(path.join(dirname, filename));
+            if (stats.birthtime < ONE_WEEK_AGO) {
+                console.log(`${filename} is too old. Deleting...`)
+                try {
+                    fs.unlinkSync(path.join(dirname, filename));
+                    console.log(`deleted ${filename}`)
+                }
+                catch (e) {
+                    console.log(`Couldn't delete ${path.join(dirname, filename)}: ${err}`);
+                }
+            }
+        });
+    }
+}
